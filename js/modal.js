09a511eae46eec67a829ecbefac3fdae92c05b7c
mdicenzo <twodogs17@icloud.com>
2019-05-03 08:42:33 -0400

    js
    js

--- js/modal.js
+++ js/modal.js
@@ -8,6 +8,7 @@ function onClick(element) {
 
 
 // Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
 
 function w3_open() {
   if (mySidebar.style.display === 'block') {
